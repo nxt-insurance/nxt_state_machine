18d0f79d94c2ba5712b6d309c6c66050dac63a3d
Andreas Robecke <a.robecke@getsafe.de>
2019-12-06 14:43:34 +0100

    Wrap TransitionProxy in NxtStatemachine module

--- lib/nxt_state_machine/transition_proxy.rb
+++ lib/nxt_state_machine/transition_proxy.rb
@@ -1,24 +1,27 @@
-class TransitionProxy
-  def initialize(context, around_callbacks = [])
-    @context = context
-    @around_callbacks = around_callbacks
-  end
-
-  def call(&proxy)
-    if around_callbacks.any?
-      around_callback_chain(proxy).call
-    else
-      proxy.call
+module NxtStateMachine
+  class TransitionProxy
+    def initialize(context, around_callbacks = [])
+      @context = context
+      @around_callbacks = around_callbacks
     end
-  end
 
-  private
-
-  def around_callback_chain(proxy)
-    around_callbacks.map { |c| NxtStateMachine::Callable.new(c).with_context(context) }.reverse.inject(proxy) do |previous, callback|
-      -> { callback.call(previous) }
+    def call(&proxy)
+      if around_callbacks.any?
+        around_callback_chain(proxy).call
+      else
+        proxy.call
+      end
     end
-  end
 
-  attr_reader :proxy, :context, :around_callbacks
+    private
+
+    def around_callback_chain(proxy)
+      around_callbacks.map { |c| Callable.new(c).with_context(context) }.reverse.inject(proxy) do |previous, callback|
+        -> { callback.call(previous) }
+      end
+    end
+
+    attr_reader :proxy, :context, :around_callbacks
+  end
 end
+
