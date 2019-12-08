72364ddf6beda3f730cf0e2fe083bb6a89f3fd91
Andreas Robecke <a.robecke@getsafe.de>
2019-12-08 14:29:34 +0100

    Introduce callback registry

---
+++ lib/nxt_state_machine/callback_registry.rb
@@ -0,0 +1,43 @@
+module NxtStateMachine
+  # TODO: This is also shit since it does not complain about misspelled keys
+  class CallbackRegistry < Registry
+    def initialize(name)
+      super(
+        "#{name}_callbacks",
+        on_key_missing: Proc.new do |callbacks, from|
+          callbacks[from] = Registry.new(
+            "#{name}_callbacks_#{from}",
+            on_key_missing: Proc.new do |from_registry, to|
+              from_registry[to] = Registry.new(
+                "#{name}_callbacks_#{from}_#{to}",
+                on_key_missing: Proc.new do |to_registry, callback_kind|
+                  to_registry[callback_kind] = []
+                end
+              )
+            end
+          )
+        end
+      )
+    end
+
+    def register(from, to, kind, method = nil, block = nil)
+      method_or_block = method || block
+      return unless method_or_block
+
+      Array(from).each do |from_state|
+        self[from_state][to][kind] << method_or_block
+      end
+    end
+
+    def resolve(transition, kind = nil)
+      @resolve ||= ActiveSupport::HashWithIndifferentAccess.new
+
+      all_callbacks = @resolve[transition.id] ||= self[transition.from][transition.to]
+      return all_callbacks unless kind
+
+      # We should check if kind is in [:before, :after, :around]
+
+      all_callbacks[kind]
+    end
+  end
+end
---
+++ spec/callback_registry_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe NxtStateMachine::CallbackRegistry do
+  subject { described_class.new('test') }
+
+  it do
+    expect(subject[:from]).to be_a(NxtStateMachine::Registry)
+    expect(subject[:from][:to]).to be_a(NxtStateMachine::Registry)
+    expect(subject[:from][:to][:before]).to be_a(Array)
+  end
+
+  describe '#register' do
+    it 'registers callbacks' do
+      subject.register([:from_first, :from_second], :to_here, :before, :a1)
+      expect(subject[:from_first][:to_here][:before]).to eq([:a1])
+      expect(subject[:from_second][:to_here][:before]).to eq([:a1])
+    end
+  end
+
+  describe '#resolve' do
+    before do
+      subject.register([:from_first, :from_second], :to_here, :before, :a1)
+    end
+
+    let(:transition) { OpenStruct.new(from: :from_second, to: :to_here) }
+
+    it 'fetches the callbacks' do
+      expect(subject.resolve(transition)[:before]).to eq([:a1])
+      expect(subject.resolve(transition)[:after]).to be_empty
+    end
+  end
+end
